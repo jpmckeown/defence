48db47d6b7de072e9d3395deb147b2bfbbbadce7
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-08-28 14:15:38 +0100

    add tile prefab and auto make tile set

---
+++ Assets/World/TileMaker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMaker : MonoBehaviour
+{
+    [SerializeField] int width = 5;
+    [SerializeField] int height = 5;
+    [SerializeField] Tile tile;
+    
+    void MakeGrid()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                var spawnedTile = Instantiate(tile, new Vector3(i,j), Quaternion.identity);
+                spawnedTile.name = $"Tile {i} {j}";
+            }
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
