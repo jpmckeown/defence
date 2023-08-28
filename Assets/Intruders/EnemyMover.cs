09b364ba4be4600aab7c59c43c11d9f4a39b14e3
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-08-28 21:22:56 +0100

    enemyMover script pasted in and edited to stop errors

---
+++ Assets/Intruders/EnemyMover.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(Enemy))]
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField][Range(0f, 5f)] float speed = 1f;
+
+    List<Node> path = new List<Node>();
+
+    //Enemy enemy;
+    GridManager gridManager;
+    Pathfinder pathfinder;
+
+    void OnEnable()
+    {
+        ReturnToStart();
+        RecalculatePath(true);
+    }
+
+    void Awake()
+    {
+        //enemy = GetComponent<Enemy>();
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
+    }
+
+    void RecalculatePath(bool resetPath)
+    {
+        Vector2Int coordinates = new Vector2Int();
+
+        if (resetPath)
+        {
+            coordinates = pathfinder.StartCoordinates;
+        }
+        else
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        }
+
+        StopAllCoroutines(); // stop enemy from following old path
+        path.Clear();
+        path = pathfinder.GetNewPath(coordinates);
+        StartCoroutine(FollowPath());
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = gridManager.GetPositionFromCoordinates(pathfinder.StartCoordinates);
+    }
+
+    void FinishPath()
+    {
+        //enemy.StealGold();
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator FollowPath()
+    {
+        // 1 not 0 stops enemy return to start of current path segment
+        // also stops them hesitating at beginning of path
+        for (int i = 1; i < path.Count; i++)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = gridManager.GetPositionFromCoordinates(path[i].coordinates);
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        FinishPath();
+    }
+}
