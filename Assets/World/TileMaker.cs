56574dfbc0155717c6381edb3474a7e74833b4d9
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-08-28 15:30:15 +0100

    tilemaker works, but vanish after run, plus camera tricky, so disabling

--- Assets/World/TileMaker.cs
+++ Assets/World/TileMaker.cs
@@ -6,20 +6,21 @@ public class TileMaker : MonoBehaviour
 {
     [SerializeField] int width = 5;
     [SerializeField] int height = 5;
+    [SerializeField] int cellSize = 10;
     [SerializeField] Tile tile;
 
     void Start()
     {
-        MakeGrid();
+        MakeTiles();
     }
 
-    void MakeGrid()
+    void MakeTiles()
     {
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                var spawnedTile = Instantiate(tile, new Vector3(i, j), Quaternion.identity);
+                var spawnedTile = Instantiate(tile, new Vector3(i * cellSize, 0, j * cellSize), Quaternion.identity);
                 spawnedTile.name = $"Tile {i} {j}";
             }
         }
