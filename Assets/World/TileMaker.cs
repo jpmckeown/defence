97a541dcdd9ea2fc522e8b7ca0501f52756b6acf
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-08-28 14:52:37 +0100

    Tile script actually exists now!

--- Assets/World/TileMaker.cs
+++ Assets/World/TileMaker.cs
@@ -7,22 +7,21 @@ public class TileMaker : MonoBehaviour
     [SerializeField] int width = 5;
     [SerializeField] int height = 5;
     [SerializeField] Tile tile;
-    
+
+    void Start()
+    {
+        MakeGrid();
+    }
+
     void MakeGrid()
     {
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                var spawnedTile = Instantiate(tile, new Vector3(i,j), Quaternion.identity);
+                var spawnedTile = Instantiate(tile, new Vector3(i, j), Quaternion.identity);
                 spawnedTile.name = $"Tile {i} {j}";
             }
-        } 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
